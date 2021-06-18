89eced9fda145142a0ee6183bc2526a84ffb4f7b
Junaid <sj.shaikhjunaid@gmail.com>
2021-06-18 17:16:24 +0530

    add nav links and make menu button work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+// get menu button and nav-links
+let menuBtn = document.querySelector("[class='bars']");
 
+let navLinks = document.querySelector(".nav-links");
+
+// show menu funtion
+function showMenu() {
+  navLinks.classList.toggle("show");
+}
+
+menuBtn.addEventListener("click", showMenu);
