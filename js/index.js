b6bbd094873c41f37476268de0c6fb6e018c5a19
Junaid <sj.shaikhjunaid@gmail.com>
2021-06-21 17:27:21 +0530

    complete toggle navigation menu

--- js/index.js
+++ js/index.js
@@ -3,9 +3,20 @@ let menuBtn = document.querySelector("[class='bars']");
 
 let navLinks = document.querySelector(".nav-links");
 
-// show menu funtion
+// toggle between the 'show' class to show and hide the navbar
 function showMenu() {
   navLinks.classList.toggle("show");
 }
 
 menuBtn.addEventListener("click", showMenu);
+
+//  close navbar when clicked ouside of navbar on mobile
+function closeMenu(e) {
+  let isClickedOutside = menuBtn.contains(e.target);
+
+  if (!isClickedOutside) {
+    navLinks.classList.remove("show");
+  }
+}
+
+document.addEventListener("click", closeMenu);
